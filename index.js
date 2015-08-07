b5d672d2021cfcdf6881cd01decea4fc3179e3ec
cereceres <jesus.edelcereceres@gmail.com>
2015-08-06 23:12:21 -0500

    randomsearch is not ok, is necessary chande the routine

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict' ;
- var D = require('./derivativenum'),
+ var D = require('./lib/derivativenum'),
   Xintersection=require('./lib/Xintersection'),
  testRoot=require('./lib/testRoot'),
  regulafalsi = require('./lib/regulafalsi'),
+  randomsearch = require('./lib/randomsearch'),
  sortInterval = require('./lib/sortInterval'),
  fixedpoint = require('./lib/fixedpoint'),
  bisection = require('./lib/bisection'),
@@ -12,7 +13,7 @@
 
 
 var nsolveqn =  function (g,interval,options) {
-    options = options || { npoints : 1000, presicion : 0.001, method : 'regulafalsi' } ;
+    options = options || { npoints : 1000, presicion : 0.001, method : 'bisection' } ;
         var presicion = options.presicion ;
         var method = options.method ;
         return  eval(' this.' +method)(g,interval,presicion);
@@ -27,3 +28,4 @@ module.exports.bisection = bisection  ;
 module.exports.Newton_Raphson = Newton_Raphson  ;
 module.exports.Newton_Raphson_Higherorder = Newton_Raphson_Higherorder  ;
 module.exports.D = D  ;
+module.exports.randomsearch = randomsearch  ;
--- prub_search.js
+++ prub_search.js
@@ -1,15 +1,18 @@
 'use strict';
 var growth = require('./bestfit') ;
-var nsolve = require('./nsolveqn') ;
-var regression = require('regression');
+var nsolve = require('./index') ;
 var pru= [[0,3.2],[1,4.6],[2,5.1],[4,6.9]];
 //console.log( growth(pru,[5,6,7]) );
 function f(x) {
-  return  Math.cos(x)-x
+  return  Math.cos(x)-x;
 }
-console.log('regulafalsi =',nsolve.regulafalsi(f,[0.0001, 100],0.0001));
-
-
-console.log('bisection =',nsolve.nsolveqn(f,[0.0000001, 100],0.0000001));
-
-console.log('fixedpoint =',nsolve.fixedpoint(f, 0.75 ,0.1));
+console.log('regulafalsi =', nsolve.regulafalsi(f,[0,1]));
+console.log('=========================================');
+console.log('bisection =', nsolve.bisection(f,[0,1]));
+console.log('=========================================');
+console.log('fixedpoint =', nsolve.fixedpoint(f,0.5));
+console.log('=========================================');
+console.log('Newton_Raphson =', nsolve.Newton_Raphson(f,[-10,10]));
+console.log('=========================================');
+console.log('randomsearch=', nsolve.randomsearch(11,[1,4,5,5.5,7,8,9,12,]));
+console.log('=========================================');
